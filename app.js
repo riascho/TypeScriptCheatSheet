50c5d245a3fe8dddd766bd7d2c7b7cc605430193
Ria <123465523+riascho@users.noreply.github.com>
2024-09-09 19:42:31 +0300

    compiles app.js

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const five = 5;
-console.log(five.toFixed(4));
+console.log("Hello There!");
+function add(n1, n2, showResult, phrase) {
+    return n1 + n2;
+}
+var num1 = 5;
+var num2 = 2.8;
+var result = add(num1, num2, true, "Result is: ");
--- app.ts
+++ app.ts
@@ -7,4 +7,4 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 const num1 = 5;
 let num2 = 2.8;
 
-const result = add(num1, num2);
+const result = add(num1, num2, true, "Result is: ");
