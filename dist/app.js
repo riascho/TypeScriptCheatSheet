db9a64b28c98b1dbfce27db527dbcb2b36a534e2
Ria <123465523+riascho@users.noreply.github.com>
2024-09-17 15:45:31 +0200

    config debug setup

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,9 @@
 "use strict";
 const button = document.querySelector("button"); // ! tells TS that button will never be null
+// if (button) { // ! type check not necessary if runtime check is done
 button.addEventListener("click", () => {
     console.log("clicked!");
     window.alert("You clicked me! :)");
 });
+// }
 //# sourceMappingURL=app.js.map
--- src/compile/app.ts
+++ src/compile/app.ts
@@ -1,6 +1,8 @@
 const button = document.querySelector("button")!; // ! tells TS that button will never be null
 
+// if (button) { // ! type check not necessary if runtime check is done
 button.addEventListener("click", () => {
   console.log("clicked!");
   window.alert("You clicked me! :)");
 });
+// }
